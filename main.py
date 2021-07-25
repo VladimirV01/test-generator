d711e91194b1cc79f4f4ba891eb518e58d555323
Vladimir <varzaru979@gmail.com>
2021-07-25 13:14:08 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,271 @@
+import random
+import sys
+from datetime import datetime, timedelta
+
+from PyQt5.QtCore import Qt, pyqtSignal
+from PyQt5.QtWidgets import QMainWindow, QApplication, QDialog, QLabel, QVBoxLayout, QHBoxLayout, QMessageBox
+
+import main_window
+
+import test_dlg
+import question_parser
+
+
+class ExtendedQLabel(QLabel):
+    def __init__(self):
+        super(ExtendedQLabel, self).__init__()
+
+    clicked = pyqtSignal(int)
+
+    def mousePressEvent(self, ev):
+        if ev.button() == Qt.LeftButton:
+            self.clicked.emit(int(self.text()))
+
+
+class TestDialog(QDialog, test_dlg.Ui_Dialog):
+    def __init__(self):
+        super(TestDialog, self).__init__()
+        self.setupUi(self)
+
+        self.start_time = 0
+        self.questions = None
+        self.question_index = 0
+        self.skipped_question_index = 0
+        self.labels = []
+        self.skipped_questions = []
+        self.finished = False
+
+        self.statistics = {
+            'right': 0,
+            'wrong': 0,
+            'accuracy': 0.0,
+            'time': 0
+        }
+
+        self.skip_btn.clicked.connect(self.skip)
+        self.next_btn.clicked.connect(self.next)
+        self.stop_btn.clicked.connect(self.stop)
+
+    def stop(self):
+        message_window = QMessageBox()
+        message_window.setIcon(QMessageBox.Warning)
+        message_window.setWindowTitle('Atentie')
+        message_window.setText('Nu ati rezolvat testul pana la sfarsit. '
+                               'Daca inchideti, progresul testului actual va fi pierdut')
+        message_window.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+        choice = message_window.exec_()
+        if choice == QMessageBox.Ok:
+            self.close()
+
+    def populate_navbar(self):
+        self.labels = [ExtendedQLabel() for _ in range(len(self.questions))]
+        vbox = QHBoxLayout()
+        self.nav_bar.setLayout(vbox)
+        for label in self.labels:
+            label.setStyleSheet('background-color : white')
+            label.setText(str(self.labels.index(label) + 1))
+            label.setAlignment(Qt.AlignCenter)
+            label.setMinimumHeight(60)
+            label.clicked.connect(self.determine_clicked_label)
+
+            vbox.addWidget(label, stretch=1)
+
+    def determine_clicked_label(self, value):
+        if value in self.skipped_questions:
+            self.reload_skipped_question(value)
+
+    def reload_skipped_question(self, value):
+        self.question_index = value - 1
+        self.load_one()
+
+    def skip(self):
+        self.labels[self.question_index].setStyleSheet('background-color : gray')
+        if self.question_index not in self.skipped_questions:
+            self.skipped_questions.append(self.question_index)
+        if self.increment_index():
+            self.load_one()
+
+    def increment_index(self):
+        if self.finished:
+            try:
+                self.question_index = self.skipped_questions[self.skipped_question_index]
+                if self.skipped_question_index + 1 < len(self.skipped_questions):
+                    self.skipped_question_index += 1
+                else:
+                    self.skipped_question_index = 0
+                return True
+            except IndexError as e:
+                # print(e)
+                return False
+        else:
+            if self.question_index + 1 < len(self.questions):
+                self.question_index += 1
+                return True
+            else:
+                self.finished = True
+            return False
+
+    def check_if_done(self):
+        if self.finished and len(self.skipped_questions) == 0:
+
+            # Calculate accuracy:
+            self.statistics['accuracy'] = float((self.statistics['right'] /
+                                                (self.statistics['right'] + self.statistics['wrong'])))
+            self.statistics['time'] = datetime.now() - self.start_time
+            message_window = QMessageBox()
+            message_window.setIcon(QMessageBox.Information)
+            message_window.setWindowTitle('Felicitari')
+            message_window.setText(f'\tAti rezolvat acest test.\n'
+                                   f'Ati acumulat {self.statistics["right"]} raspunsuri corect si '
+                                   f'{self.statistics["wrong"]} raspunsuri gresite. \nInsusire intrebarilor'
+                                   f' este de {self.statistics["accuracy"]*100}%.\n'
+                                   f'Rezolvarea testului a durat {str(self.statistics["time"])}.')
+            message_window.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+            choice = message_window.exec_()
+            if choice == QMessageBox.Ok:
+                self.close()
+
+    def next(self):
+        if self.var_a.isChecked() or self.var_b.isChecked() or self.var_c.isChecked() or self.var_d.isChecked():
+            if self.var_a.isChecked() and self.questions[self.question_index].get_answer() == 'a':
+                self.labels[self.question_index].setStyleSheet('background-color : green')
+                self.statistics['right'] += 1
+            elif self.var_b.isChecked() and self.questions[self.question_index].get_answer() == 'b':
+                self.labels[self.question_index].setStyleSheet('background-color : green')
+                self.statistics['right'] += 1
+            elif self.var_c.isChecked() and self.questions[self.question_index].get_answer() == 'c':
+                self.labels[self.question_index].setStyleSheet('background-color : green')
+                self.statistics['right'] += 1
+            elif self.var_d.isChecked() and self.questions[self.question_index].get_answer() == 'c':
+                self.labels[self.question_index].setStyleSheet('background-color : green')
+                self.statistics['right'] += 1
+            else:
+                self.labels[self.question_index].setStyleSheet('background-color : red')
+                self.statistics['wrong'] += 1
+
+            if self.question_index in self.skipped_questions:
+                self.skipped_questions.remove(self.question_index)
+                if self.skipped_question_index > 0:
+                    self.skipped_question_index -= 1
+            if self.increment_index():
+                self.load_one()
+
+        else:
+            message_window = QMessageBox()
+            message_window.setIcon(QMessageBox.Warning)
+            message_window.setWindowTitle('Atentie')
+            message_window.setText('Nu ati selectat nici o varianta.')
+            message_window.setStandardButtons(QMessageBox.Ok)
+            message_window.exec_()
+        self.check_if_done()
+
+    def load_one(self):
+        self.question_number.display(self.question_index + 1)
+        self.load_question(self.questions[self.question_index])
+        self.highlight_current_question_in_navbar()
+
+    def highlight_current_question_in_navbar(self):
+        self.labels[self.question_index].setStyleSheet('background-color : lightgray')
+
+    def load_question(self, question):
+        self.question_nr.setText(f'Intrebarea numarul {str(question.get_question_nr())} '
+                                 f'din {question.get_domain_nr()} - {question.get_domain_name()}')
+        self.question_text.clear()
+        self.question_text.insertPlainText(question.get_question_text())
+        variants = question.get_variants()
+        self.var_a_text.clear()
+        self.var_b_text.clear()
+        self.var_c_text.clear()
+        self.var_d_text.clear()
+        self.var_a_text.insertPlainText(variants[0])
+        self.var_b_text.insertPlainText(variants[1])
+        self.var_c_text.insertPlainText(variants[2])
+        self.var_d_text.insertPlainText(variants[3])
+
+    def run_test(self, questions):
+        self.start_time = datetime.now()
+        self.questions = questions
+        self.populate_navbar()
+        self.load_one()
+        self.exec_()
+        return self.statistics
+
+
+class Root(QMainWindow, main_window.Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(Root, self).__init__(parent)
+        self.setupUi(self)
+
+        # Get the question from file.
+        parser = question_parser.Parser()
+        # Set up a statistics variable.
+        self.statistics = {}
+        self.domenii = []
+        for i in range(4):
+            parser.set_path(f'Domeniul_{i + 1}.txt', f'Answers_{i+1}.txt')
+            parser.load_info()
+            parser.process_info()
+            parser.process_answers()
+            self.domenii.append(question_parser.Domeniu(parser.get_info(), parser.get_answer_list()))
+        for i in self.domenii:
+            i.extract_questions()
+
+        self.generate.clicked.connect(self.show_test_window)
+
+    def check_selected_domains(self):
+        dom_choices = []
+        if self.dom_I.isChecked():
+            dom_choices.append(self.domenii[0])
+        if self.dom_II.isChecked():
+            dom_choices.append(self.domenii[1])
+        if self.dom_II.isChecked():
+            dom_choices.append(self.domenii[2])
+        if self.dom_IV.isChecked():
+            dom_choices.append(self.domenii[3])
+        return dom_choices
+
+    def get_random_domain(self):
+        choices = self.check_selected_domains()
+        return random.choice(choices)
+
+    def reset_extracted_questions_contor(self):
+        for i in self.domenii:
+            i.reset_extracted_question_contor()
+
+    def get_questions(self, nr=1):
+        questions = []
+        for i in range(nr):
+            domain = self.get_random_domain()
+            questions.append(domain.get_random_question())
+        self.reset_extracted_questions_contor()
+        return questions
+
+    def get_questions_number(self):
+        if self.tests_20.isChecked():
+            number = 20
+        elif self.tests_30.isChecked():
+            number = 30
+        else:
+            number = 40
+        return number
+
+    def show_test_window(self):
+        questions = self.get_questions(self.get_questions_number())
+
+        window = TestDialog()
+        self.statistics = window.run_test(questions)
+        # TODO: Show last test results in the main window.
+        # TODO: Implement the inspector window.
+        # TODO: Add the functionality to show the statistics graph.
+        # TODO: Add the function to write and read internal data to file.
+
+
+def main():
+    application = QApplication(sys.argv)
+    window = Root()
+    window.show()
+    application.exec_()
+
+
+if __name__ == "__main__":
+    main()
---
+++ question_parser.py
@@ -0,0 +1,184 @@
+import random
+
+
+class Parser:
+    def __init__(self, path=None, answers_path=None):
+        self.path = path
+        self.answers_path = answers_path
+        self.info = None
+        self.answer_list = []
+
+    def set_path(self, path, answers_path):
+        self.path = path
+        self.answers_path = answers_path
+
+    def load_info(self):
+        with open(self.path, 'r') as file:
+            lines = file.readlines()
+        self.info = lines
+
+    def process_info(self):
+        # Remove newlines from the list
+        while '\n' in self.info:
+            self.info.remove('\n')
+
+        # Replace tabs in the strings with simple spaces
+        for i in range(len(self.info)):
+            self.info[i] = self.info[i].replace('\t', ' ')
+
+    def process_answers(self):
+        with open(self.answers_path, 'r') as file:
+            answers = file.readlines()
+        for i in answers:
+            self.answer_list.append(i.split(' ')[1].replace('\n', ''))
+
+    def get_info(self):
+        return self.info
+
+    def get_answer_list(self):
+        return self.answer_list
+
+
+class Domeniu:
+    def __init__(self, info, answer_list):
+        self.domain_nr = info[0]
+        self.domain_name = info[1]
+        self.info = info[2:]
+        self.answer_list = answer_list
+
+        self.questions = []
+        self.extracted_questions = []
+
+    def extract_questions(self):
+        index = 0
+        while index < len(self.info):
+            try:
+                if int(self.info[index][0]) in (1, 2, 3, 4, 5, 6, 7, 8, 9):
+                    new_question = self.Question(self.info[index],
+                                                 self.info[index + 1],
+                                                 self.info[index + 2],
+                                                 self.info[index + 3],
+                                                 self.info[index + 4],
+                                                 self.domain_nr,
+                                                 self.domain_name,
+                                                 self.answer_list)
+                    self.questions.append(new_question)
+                    index += 5
+            except ValueError:
+                index += 1
+
+    def get_random_question(self):
+
+        question = random.choice(self.questions)
+        while question.get_question_nr() in self.extracted_questions:
+            question = random.choice(self.questions)
+
+        self.extracted_questions.append(question.get_question_nr())
+        return question
+
+    def get_specific_question(self, nr):
+        for question in self.questions:
+            if question.get_question_nr() == nr:
+                return question
+
+    def get_extracted_question_list(self):
+        return self.extracted_questions
+
+    def reset_extracted_question_contor(self):
+        self.extracted_questions = []
+
+    def get_all_questions(self):
+        return self.questions
+
+    class Question:
+        def __init__(self, text, var_a, var_b, var_c, var_d, domain_nr, domain_name, answers):
+            self.question_text = text
+            self.question_nr = None
+            self.domain_nr = domain_nr
+            self.domain_name = domain_name
+            self.extract_question_number()
+            # Slice the first 3 chars from the variants. Those chars are a), b), c), d).
+            self.var_a = var_a[3:]
+            self.var_b = var_b[3:]
+            self.var_c = var_c[3:]
+            self.var_d = var_d[3:]
+
+            self.answer = None
+            self.answers = answers
+            self.find_answer()
+
+        def find_answer(self):
+            self.answer = self.answers[self.question_nr - 1]
+
+        def extract_question_number(self):
+            char_nr = ''
+            for i in range(len(self.question_text)):
+                if self.question_text[i] != '.':
+                    # Collect the string containing question number until '.' found which states for the delimitation
+                    # of the question number.
+                    char_nr += self.question_text[i]
+                elif self.question_text[i] == '.':
+                    # Convert the collected question number from string to number.
+                    self.question_nr = int(char_nr)
+                    # Slice the number from the question text.
+                    self.question_text = self.question_text[(i + 2):]
+                    break
+
+        def set_domain_nr(self, info):
+            self.domain_nr = info
+
+        def get_domain_nr(self):
+            return self.domain_nr
+
+        def set_domain_name(self, info):
+            self.domain_name = info
+
+        def get_domain_name(self):
+            return self.domain_name
+
+        def set_right_answer(self, answer):
+            self.answer = answer
+
+        def get_question_text(self):
+            return self.question_text
+
+        def get_variants(self):
+            return self.var_a, self.var_b, self.var_c, self.var_d
+
+        def get_answer(self):
+            return self.answer
+
+        def get_question_nr(self):
+            return self.question_nr
+
+        def print_question(self):
+            print(f'Question number {self.question_nr}: {self.question_text}')
+            print(f'a) {self.var_a}')
+            print(f'b) {self.var_b}')
+            print(f'c) {self.var_c}')
+            print(f'd) {self.var_d}')
+            print(f'Answer: {self.answer}')
+
+
+if __name__ == "__main__":
+    parser = Parser('Domeniul_1.txt', 'Answers_1.txt')
+    parser.load_info()
+    parser.process_info()
+    parser.process_answers()
+    dom1 = Domeniu(parser.get_info(), parser.get_answer_list())
+    dom1.extract_questions()
+    dom1.get_random_question().print_question()
+    dom1.get_specific_question(57).print_question()
+
+    # parser.set_path('Domeniul_2.txt')
+    # parser.load_info()
+    # parser.process_info()
+    # parser.process_answers()
+    # dom2 = Domeniu(parser.get_info(), parser.get_answer_list())
+    # dom2.extract_questions()
+    # dom2.get_random_question().print_question()
+    # dom2.get_specific_question(24).print_question()
+
+
+
+
